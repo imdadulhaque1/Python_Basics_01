a1e291cfda69462662e82c549a8cd3254b7ca21b
Imdadul Haque <45633928+imdadulhaque1@users.noreply.github.com>
2020-01-29 13:51:14 +0600

    Python Learning

--- Faulty_Calculator.py
+++ Faulty_Calculator.py
@@ -1,55 +1,107 @@
+#Fault ResuLt : 45 * 3==555 & 56 + 9==77 & 56 / 6==4 
+while True:
+    print("\n\n+ for addition")
+    print("- for subtraction")
+    print("* for multiplication")
+    print("/ for division")
+    print("** for power")
+    print("% for modulus")
 
-#Fault ResuLt : 45 * 3==555 & 56 + 9==77 & 56 / 6==4
-print("Fault ResuLt : 45 * 3==555 & 56 + 9==77 & 56 / 6==4")
+    op = input("\nEnter your choice: ")
+    if op =='+':
+        n1 = int(input("\nEnter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        if n1==56 and n2==9:
+            print(f"{n1} + {n2} = 77")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
+        else:
+            add = n1 + n2
+            print(f"{n1} + {n2} = {add}")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
+    elif op=='-':
+        n1 = int(input("Enter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        sub = n1 - n2
+        print(f"{n1} - {n2} = {sub}")
+        
+        again = input("Do you want to calculate again 'Yes \ No' ? ")
+        if again == 'Yes':
+            continue
+        else:
+            break
 
-print("+ for addition")
-print("- for subtraction")
-print("* for multiplication")
-print("/ for division")
-print("** for power")
-print("% for modulus")
+    elif op=='*':
+        n1 = int(input("Enter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        if n1==45 and n2==3:
+            print(f"{n1} + {n2} = 555")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
+        else:
+            multi = n1 * n2
+            print(f"{n1} * {n2} = {multi}")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
 
-op = input("\nEnter your choice: ")
-if op =='+':
-    n1 = int(input("\nEnter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    if n1==56 and n2==9:
-        print(f"{n1} + {n2} = 77")
-    else:
-        add = n1 + n2
-        print(f"{n1} + {n2} = {add}")
-elif op=='-':
-    n1 = int(input("Enter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    sub = n1 - n2
-    print(f"{n1} - {n2} = {sub}")
-    
-elif op=='*':
-    n1 = int(input("Enter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    if n1==45 and n2==3:
-        print(f"{n1} + {n2} = 555")
-    else:
-        multi = n1 * n2
-        print(f"{n1} * {n2} = {multi}")
+    elif op=='/':
+        n1 = int(input("Enter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        if n1==56 and n2==6:
+            print(f"{n1} / {n2} = 4")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
+        else:
+            div = n1 / n2
+            print(f"{n1} / {n2} = {div}")
+            
+            again = input("Do you want to calculate again 'Yes \ No' ? ")
+            if again == 'Yes':
+                continue
+            else:
+                break
 
-elif op=='/':
-    n1 = int(input("Enter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    if n1==56 and n2==6:
-        print(f"{n1} / {n2} = 4")
-    else:
-        div = n1 / n2
-        print(f"{n1} / {n2} = {div}")
-    
-elif op=='**':
-    n1 = int(input("Enter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    po = n1 ** n2
-    print(f"{n1} ** {n2} = {po}")
+    elif op=='**':
+        n1 = int(input("Enter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        po = n1 ** n2
+        print(f"{n1} ** {n2} = {po}")
+        
+        again = input("Do you want to calculate again 'Yes \ No' ? ")
+        if again == 'Yes':
+            continue
+        else:
+            break
 
-elif op=='%':
-    n1 = int(input("Enter first Number: "))
-    n2 = int(input("Enter second Number: "))
-    po = n1 % n2
-    print(f"{n1} % {n2} = {po}")
+    elif op=='%':
+        n1 = int(input("Enter first Number: "))
+        n2 = int(input("Enter second Number: "))
+        po = n1 % n2
+        print(f"{n1} % {n2} = {po}")
+        
+        again = input("Do you want to calculate again 'Yes \ No' ? ")
+        if again == 'Yes':
+            continue
+        else:
+            break
